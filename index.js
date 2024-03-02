77c999451c0309e377a6aaeba2834e710b513a14
luisdev1998 <luis.dev1998@gmail.com>
2024-03-02 14:48:07 -0500

    a

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ import WebRoutes from "./routes/WebRoutes.js";
 const app = express();
 app.use(express.json());
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 //Database
 db.authenticate()
     .then( () => console.log("base de datos conectado"))
